b6c5b88abbd81e9b50758b21a77edb0c0ad05c67
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-11-21 11:49:33 +0530

    second

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import Map from "./Map";
 const App = () => {
-  return <div>Hello this is MAP </div>;
+  return (
+    <div>
+      Hello this is MAP
+      <Map />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+
+const API_KEY = "AIzaSyBG7hTxAymvTF0a1kra5kKDBMF9nqBmKdc";
 
 const Map = () => {
-  return <div>This is where map should be</div>;
+  const libraries = ["places"];
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: API_KEY,
+    libraries,
+  });
+
+  const Mapstyle = {
+    height: "50vh",
+    width: "50wh",
+    margin: "auto",
+  };
+
+  const center = {
+    lat: 43.6532,
+    lng: -79.3832,
+  };
+
+  if (loadError) {
+    return (
+      <div>
+        Error loading map
+        {console.log(loadError)}
+      </div>
+    );
+  }
+
+  if (!isLoaded) {
+    return <div>Loading</div>;
+  }
+
+  return (
+    <div>
+      {console.log(process.env.REACT_APP_GOOGLE_MAPS_API)}
+      This is where map should be
+      <GoogleMap
+        mapContainerStyle={Mapstyle}
+        zoom={8}
+        center={center}
+      ></GoogleMap>
+    </div>
+  );
 };
 
 export default Map;
