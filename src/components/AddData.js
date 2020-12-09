705a321f7b61b1272ba43830544a748b5e37859e
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-12-09 13:19:01 +0530

    add data

---
+++ src/components/AddData.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
+
+export default function TransitionsModal({ open, setOpen, data, setData }) {
+  const classes = useStyles();
+  const [info, setInfo] = useState({});
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    setData([...data, info]);
+  };
+
+  const inputChange = (e, name) => {
+    if (name === "Latitude" || name === "Longitude") {
+      setInfo({
+        ...info,
+        [name]: parseFloat(e.target.value) * 1,
+        id: data.length + 1,
+      });
+    }
+    setInfo({ ...info, [name]: e.target.value, id: data.length + 1 });
+  };
+
+  return (
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <form
+              className={classes.root}
+              autoComplete="off"
+              style={{ display: "flex", flexDirection: "column" }}
+              onSubmit={onFormSubmit}
+            >
+              <TextField
+                className="standard-basic"
+                label="Place"
+                required
+                onChange={(e) => inputChange(e, "Name")}
+              />
+              <TextField
+                className="standard-basic"
+                label="PhotoUrl"
+                required
+                onChange={(e) => inputChange(e, "image")}
+              />
+              <input
+                className="outlined-basic"
+                label="latitude"
+                variant="outlined"
+                required
+                type="number"
+                step="any"
+                onChange={(e) => inputChange(e, "Latitude")}
+              />
+              <input
+                type="number"
+                label="longitude"
+                variant="outlined"
+                required
+                step="any"
+                onChange={(e) => inputChange(e, "Longitude")}
+              />
+
+              <Button variant="contained" color="primary" type="submit">
+                Add Place
+              </Button>
+            </form>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
--- src/components/App.js
+++ src/components/App.js
@@ -2,36 +2,27 @@ import React, { useState } from "react";
 import Map from "./Map";
 import Places from "./Places";
 import "../Styling/index.css";
+import AddData from "./AddData";
 import { data } from "./data";
 import Path from "./Path";
 
 const App = () => {
   const [dark, setDark] = useState(false);
   const [places, setPlaces] = useState([...data]);
-
+  const [open, setOpen] = useState(false);
   return (
     <div>
       <button onClick={() => setDark(!dark)}>Dark</button>
       <Map dark={dark} data={places} />
       <Places data={places} />
       <Path data={places} />
-      <button
-        onClick={() =>
-          setPlaces([
-            ...places,
-            {
-              Name: "Kalyan",
-              Latitude: 19.24,
-              Longitude: 73.123,
-              image:
-                "https://upload.wikimedia.org/wikipedia/commons/9/9f/Kalyan_Junction_railway_station_-_Stationboard.jpg",
-              id: places.length + 1,
-            },
-          ])
-        }
-      >
-        Add
-      </button>
+      <button onClick={() => setOpen(true)}>Add</button>
+      <AddData
+        open={open}
+        setOpen={setOpen}
+        data={places}
+        setData={setPlaces}
+      />
     </div>
   );
 };
